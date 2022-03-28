675ae61c6a6cceccfce1dcda4dde850ba8921718
TheJPMZ <monzon.jpmz@gmail.com>
2022-03-28 08:39:44 -0600

    ➕ Background, css and asset management

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Testing</h1>
+                <h1>New Test</h1>
+                Wow nice testaaaaaa
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: './src/index.js',
@@ -16,11 +17,25 @@ module.exports = {
                     loader: 'babel-loader',
                 }
             },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif|mp4)$/i,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'visual/',
+                },
+            },
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
         })
     ]
 };
