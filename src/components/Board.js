9812295fbe25ae0087f18e8f4b243d918764549c
TheJPMZ <monzon.jpmz@gmail.com>
2022-04-05 08:22:12 -0600

    ➕ Added a random function for the board

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,11 +4,23 @@ import "./Board.css"
 
 class Board extends Component {
 
+    constructor(props) {
+        super(props);
+        this.lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+        const shuffled = function (list) {
+            return list.map(x => ({x, sort: Math.random()})).sort((a, b) => a.sort - b.sort).map(({x}) => x)
+        }
+
+        this.lista = shuffled(this.lista)
+    }
 
     render() {
         return (
             <div className="wrapper">
-                A
+                {this.lista.map((x)=> (
+                    <div> {x}</div>
+                ))}
             </div>
         );
     }
