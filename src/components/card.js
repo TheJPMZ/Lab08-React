31320f7d21efb4586b4482445579446c815b53ad
TheJPMZ <monzon.jpmz@gmail.com>
2022-04-07 01:23:14 -0600

    ➕ Cards now behave correctly, now you can win the game

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from "react";
 import './card.css'
-import audio from '../assets/Card-flip-sound-effect.mp3'
+import cardaudio from '../assets/Card-flip-sound-effect.mp3'
+import dingaudio from '../assets/ding-sound-effect_2.mp3'
+import useStateManager from './statemanager'
 
 
 const images = ["https://s3.duellinksmeta.com/cards/60c2b3aca0e24f2d54a5314c_w420.webp",
@@ -13,18 +15,52 @@ const images = ["https://s3.duellinksmeta.com/cards/60c2b3aca0e24f2d54a5314c_w42
                 "https://s3.duellinksmeta.com/cards/60c2b3aca0e24f2d54a5314d_w420.webp",
                 "https://s3.duellinksmeta.com/cards/60c2b3aca0e24f2d54a5314b_w420.webp"]
 
-export default function Card({image}){
+const Card = ({image}) => {
+
+    const {pair, checkLeft, checkLogLength, checkLog, removeLog, addLog} = useStateManager();
 
     const string = "url(" + images[image] + ")"
 
     const [cardState, cardRotate] = useState(false);
 
-    const card_audio = new Audio(audio)
+    const card_audio = new Audio(cardaudio)
     card_audio.volume = 0.2;
+    const ding_audio = new Audio(dingaudio)
+    ding_audio.volume = 0.2;
 
     const rotate = () => {
-        cardRotate((cardState)=>!cardState);
-        card_audio.play()
+
+        const flip = () => {
+            card_audio.play()
+            cardRotate((cardState) => !cardState);
+        }
+
+        if (!checkLeft(image)){
+            console.log("Ya haz hecho esta pareja")
+            return
+        }
+
+        if (checkLogLength() && !cardState){
+            return
+        }
+
+        //Si la carta no esta girada
+        flip()
+        if (!cardState) {
+            //Checkeamos is la carta esta en el log
+            //Si esta en el log, la borramos del log y la giramos hemos hecho una pareja
+            if (checkLog(image)) {
+                ding_audio.play()
+                pair(image)
+            //Si no esta en el log la agregamos y le damos vuelta
+            } else {
+                addLog(image)
+            }
+
+        //Si la carta esta girada, la giramos y la quitamos del log y ponemos el sonido
+        } else {
+            removeLog(image);
+        }
     }
 
     return (
@@ -36,8 +72,7 @@ export default function Card({image}){
         </div>
     )
 
-
-
-
 }
+
+export default Card;
 
